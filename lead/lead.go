a269a14146dcbac88f4143bd1c3a8ba0037fcaa3
Aakashraz <aakashraz@outlook.com>
2024-03-03 00:09:04 +0545

    added get and create function

--- lead/lead.go
+++ lead/lead.go
@@ -16,7 +16,13 @@ type Lead struct {
 }
 
 func GetLeads(c *fiber.Ctx) {
-	//...
+	db := database.Db
+	var leads []Lead
+	db.Find(&leads)
+	err := c.JSON(leads)
+	if err != nil {
+		log.Printf("error while serializing and sending JSON response: %v", err)
+	}
 }
 
 func GetLead(c *fiber.Ctx) {
@@ -25,10 +31,30 @@ func GetLead(c *fiber.Ctx) {
 	var ld Lead
 	//Executes a database query to find a lead with the specified ID (value of the "id" parameter) and stores the result in the ld variable.
 	db.Find(&ld, id)
-	//Uses the c.JSON method provided by the Fiber framework to serialize the ld (Lead) struct into JSON format and send it as the response.
+	//Uses the c.JSON method provided by the Fiber framework
+	//to serialize the ld (Lead) struct into JSON format and send it as the response.
 	err := c.JSON(ld)
 	if err != nil {
-		log.Printf("error while reading json: %v", err)
+		log.Printf("error while serializing and sending JSON response: %v", err)
 		return
 	}
+}
+
+func NewLead(c *fiber.Ctx) {
+	db := database.Db
+	//The new function is used to allocate memory for a new	  zero-initialized value of the specified type.
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.CreateTable(&lead)
+	err := c.JSON(lead)
+	if err != nil {
+		log.Printf("error while serializing and sending JSON response: %v", err)
+	}
+}
+
+func DeleteLead(c *fiber.Ctx) {
+	//
 }
